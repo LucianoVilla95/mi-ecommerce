d0c68ac9f0a4548b81b45fcc30aa61329e418e1b
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-03-05 22:14:32 -0300

    feat: add users repository

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,7 @@
 import { Controller, Post,Get, Put, Delete, Body } from '@nestjs/common';
+import { UsersBodyDto } from './dtos/usersBodyDto.dto';
 
 @Controller('users')
 export class UsersController {
   constructor () {}
-
-  @Post()
-  async createUser(@Body() {name, email, password, phone, country, address, city}) {
-
-  }
 }
