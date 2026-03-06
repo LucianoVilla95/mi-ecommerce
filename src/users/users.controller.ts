b2b6fad96b16dc61b98005dddba7f3e939441ef8
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-03-05 21:30:18 -0300

    feat: add users entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post,Get, Put, Delete, Body } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
---
+++ src/users/users.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity({
+    name: 'users'
+})
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string = uuid();
+
+    @Column({length: 50, nullable: false})
+    name: string;
+
+    @Column({length: 50, nullable: false, unique: true})
+    email: string;
+
+    @Column({length: 20, nullable: false})
+    password: string;
+
+    @Column({length: 20})
+    phone: string;
+
+    @Column({length: 50})
+    country: string;
+
+    @Column()
+    address: string;
+
+    @Column({length: 50})
+    city: string;
+}
