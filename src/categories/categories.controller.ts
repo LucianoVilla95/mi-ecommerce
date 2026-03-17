da8455e727db730a5f598ba59cb7b62af709a5c8
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-03-17 02:28:17 -0300

    feat(categories): add guards and role decorator to POST endpoint

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesBodyDto } from './dtos/categoriesBodyDto.dto';
 import { Category } from './categories.entity';
+import { JwtAuthGuard } from 'src/guards/auth.guard';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { Roles } from 'src/decorators/rolesUser.decorator';
 
 @Controller('categories')
 export class CategoriesController {
   constructor (private readonly categoriesService: CategoriesService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   @Post()
   async createCategory(@Body() body: CategoriesBodyDto): Promise<Category> {
     return await this.categoriesService.createCategory(body);
