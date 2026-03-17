70470189d18f93c2c5b06201caab0d55ffd7100a
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-03-17 02:31:52 -0300

    feat(categories): implement get category by id endpoint

--- src/categories/categories.repository.ts
+++ src/categories/categories.repository.ts
@@ -7,17 +7,22 @@ import { Repository } from "typeorm";
 export class CategoriesRepository {
   constructor (@InjectRepository(Category) private readonly categoriesRepository: Repository<Category>) {}
 
-  async getCategoryByName(formattedName: string): Promise<Category | null> {
-    return await this.categoriesRepository.findOne({where: {name: formattedName}});
+  async getCategoryByName(name: string): Promise<Category | null> {
+    return await this.categoriesRepository.findOne({where: {name}});
   }
 
-  async createCategory(formattedName: string, slug: string): Promise<Category> {
-    const category: Category = await this.categoriesRepository.create({name: formattedName, slug});
+  async createCategory(name: string, slug: string): Promise<Category> {
+    const category: Category = await this.categoriesRepository.create({name, slug});
     return await this.categoriesRepository.save(category);
   }
 
   async getCategories(): Promise<Category[]> {
     const categories: Category[] = await this.categoriesRepository.find();
     return categories;
+  }
+
+  async getCategoryById(id: string): Promise<Category | null> {
+    const category: Category | null = await this.categoriesRepository.findOne({ where: {id} });
+    return category; 
   }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException, InternalServerErrorException } from "@nestjs/common";
+import { Injectable, BadRequestException, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import { CategoriesRepository } from "./categories.repository";
 import { CategoriesBodyDto } from "./dtos/categoriesBodyDto.dto";
 import { Category } from "./categories.entity";
@@ -9,20 +9,14 @@ export class CategoriesService {
   constructor (private readonly categoriesRepository: CategoriesRepository) {}
 
   async createCategory({name}: CategoriesBodyDto): Promise<Category> {
-    const normalizedName: string = name.trim().toLowerCase();
-
-    const formattedName: string = normalizedName
-      .split(' ')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
     try {
-      const categoryExists: Category | null = await this.categoriesRepository.getCategoryByName(formattedName);
+      const categoryExists: Category | null = await this.categoriesRepository.getCategoryByName(name);
       
       if (categoryExists) throw new BadRequestException('Category already exists');
 
-      const slug: string = slugify(formattedName, {lower: true, strict: true});
+      const slug: string = slugify(name, {lower: true, strict: true});
 
-      return await this.categoriesRepository.createCategory(formattedName, slug);
+      return await this.categoriesRepository.createCategory(name, slug);
       
     } catch (error) {
       console.log(error);
@@ -35,5 +29,13 @@ export class CategoriesService {
 
   async getCategories(): Promise<Category[]> {
     return await this.categoriesRepository.getCategories();
+  }
+
+  async getCategoryById(id: string): Promise<Category | null> {
+    const category: Category | null = await this.categoriesRepository.getCategoryById(id);
+
+    if (!category) throw new NotFoundException('Category not found');
+
+    return category;
   }
 }
