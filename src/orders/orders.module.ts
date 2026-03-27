e9ebf7cde1bae9a02abb3c2bc4ecc0d7964b0cfd
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-03-27 02:31:18 -0300

    feat(orders): configure module to use external services

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,9 +4,12 @@ import { OrdersService } from './orders.service';
 import { OrdersRepository } from './orders.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './orders.entity';
+import { OrderDetail } from './orderDetails.entity';
+import { UsersModule } from 'src/users/users.module';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [TypeOrmModule.forFeature([Order, OrderDetail]), UsersModule, ProductsModule],
   controllers: [OrdersController],
   providers: [OrdersService, OrdersRepository]
 })
