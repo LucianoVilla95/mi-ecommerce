2263afda6e8b09fad1f1a30569cd047f6bbfc055
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-03-27 02:22:19 -0300

    feat(orders): add POST endpoint to add products

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { AddProductDto } from './dtos/addProductDto.dto';
+import { JwtAuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/decorators/currentUser.decorator';
+import type { JwtPayload } from 'src/users/interfaces/jwtPayload.interface';
 
-Controller('orders')
+@Controller('orders')
 export class OrdersController {
-  constructor () {}
+  constructor (private readonly ordersService: OrdersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  addProduct(@Body() body: AddProductDto, @CurrentUser() user: JwtPayload) {
+    return this.ordersService.addProduct(user, body);
+  }
 }
