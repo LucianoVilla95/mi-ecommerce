b7c02bb3b32f174147ba13689a98db8796e4c55d
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-03-28 04:39:05 -0300

    feat(orders): add update and delete endpoints for cart products

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, UseGuards, ParseUUIDPipe, Patch } from '@nestjs/common';
 import { OrdersService } from './orders.service';
-import { AddProductDto } from './dtos/addProductDto.dto';
+import { AddProductsDto } from './dtos/addProductDto.dto';
 import { JwtAuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/decorators/currentUser.decorator';
 import type { JwtPayload } from 'src/users/interfaces/jwtPayload.interface';
+import { RemoveProductsDto } from './dtos/removeProductDto.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -11,7 +12,19 @@ export class OrdersController {
 
   @UseGuards(JwtAuthGuard)
   @Post()
-  addProduct(@Body() body: AddProductDto, @CurrentUser() user: JwtPayload) {
-    return this.ordersService.addProduct(user, body);
+  async addProduct(@Body() body: AddProductsDto, @CurrentUser() user: JwtPayload): Promise<{message: string}> {
+    return await this.ordersService.addProduct(user, body);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  async updateQuantity(@Param('id', new ParseUUIDPipe({ version: '4'})) id: string, @CurrentUser() user: JwtPayload, @Body() body: RemoveProductsDto): Promise<{message: string}> {
+    return await this.ordersService.updateQuantity(user, id, body);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async removeProduct(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string, @CurrentUser() user: JwtPayload): Promise<{message: string}> {
+    return await this.ordersService.removeProduct(id, user.sub);
   }
 }
