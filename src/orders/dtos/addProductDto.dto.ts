5d317a854403b6bdc406868d84e292d3dc38d959
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-03-28 04:36:45 -0300

    fix(cart): update add product DTO

--- src/orders/dtos/addProductDto.dto.ts
+++ src/orders/dtos/addProductDto.dto.ts
@@ -1,7 +1,7 @@
 import { IsInt, IsNotEmpty, IsString, IsUUID, Min } from "class-validator";
 import { Type } from "class-transformer";
 
-export class AddProductDto {
+export class AddProductsDto {
   @IsString()
   @IsNotEmpty()
   @IsUUID('4')
@@ -9,7 +9,7 @@ export class AddProductDto {
 
   @IsNotEmpty()
   @IsInt()
-  @Min(1)
+  @Min(0)
   @Type(() => Number)
   quantity: number;
 }
