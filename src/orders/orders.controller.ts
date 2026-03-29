1eac50e60be86ca1d7eaacb8efb7d15ddaaf8101
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-03-29 03:59:30 -0300

    feat(orders): add endpoint to retrieve user cart with nested relations

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Delete, Param, Post, UseGuards, ParseUUIDPipe, Patch } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, UseGuards, ParseUUIDPipe, Patch, Get } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { AddProductsDto } from './dtos/addProductDto.dto';
 import { JwtAuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/decorators/currentUser.decorator';
 import type { JwtPayload } from 'src/users/interfaces/jwtPayload.interface';
 import { RemoveProductsDto } from './dtos/removeProductDto.dto';
+import { Order } from './orders.entity';
 
 @Controller('orders')
 export class OrdersController {
@@ -26,5 +27,11 @@ export class OrdersController {
   @Delete(':id')
   async removeProduct(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string, @CurrentUser() user: JwtPayload): Promise<{message: string}> {
     return await this.ordersService.removeProduct(id, user.sub);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getCart(@CurrentUser() user: JwtPayload): Promise<Omit<Order, 'user' | 'date' | 'isActive'> & {total: number}> {
+    return await this.ordersService.getOrder(user);
   }
 }
