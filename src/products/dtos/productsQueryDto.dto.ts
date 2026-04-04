d12427871afec5626b42ef7b628ae6d3c1c40bff
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-04-04 02:29:04 -0300

    fix(products): update import paths

--- src/products/dtos/productsQueryDto.dto.ts
+++ src/products/dtos/productsQueryDto.dto.ts
@@ -1,3 +1,3 @@
-import { UsersQueryDto } from "src/users/dtos/usersQueryDto.dto";
+import { UsersQueryDto } from "../../users/dtos/usersQueryDto.dto";
 
 export class ProductsQueryDto extends UsersQueryDto {}
