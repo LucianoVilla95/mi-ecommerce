7c49883a2ddb338d7cd490bff10692fd6c02f6fc
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-05-17 20:30:05 -0300

    fix: update dtos to resolve typing issues

--- api/src/users/dtos/usersUpdateDto.dto.ts
+++ api/src/users/dtos/usersUpdateDto.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from "@nestjs/swagger";
 import { UsersBodyDto } from "./usersBodyDto.dto";
+import { IsDate, IsOptional, IsString, ValidateIf } from "class-validator";
+import { Type } from "class-transformer";
 
-export class UsersUpdateDto extends PartialType(UsersBodyDto) {};
+export class UsersUpdateDto extends PartialType(UsersBodyDto) {
+  @IsOptional()
+  @ValidateIf((Object, value) => value !== null)
+  @IsString()
+  resetToken?: string | null;
+
+  @IsOptional()
+  @ValidateIf((Object, value) => value !== null)
+  @IsDate()
+  @Type(() => Date)
+  resetTokenExpires?: Date | null;
+};
