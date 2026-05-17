2012dc61445fd35b8ae2533b78940324b1442679
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-05-17 19:28:58 -0300

    feat: import resend module into users module

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
+import { ResendModule } from '../resend/resend.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), ResendModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersService]
