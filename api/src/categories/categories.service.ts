b734d6a8852f88ac905ea1880e89d8de2cbade2f
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-05-18 19:15:51 -0300

    feat(categories): implement category update service

--- api/src/categories/categories.service.ts
+++ api/src/categories/categories.service.ts
@@ -1,14 +1,19 @@
-import { Injectable, BadRequestException, InternalServerErrorException, NotFoundException } from "@nestjs/common";
+import { Injectable, BadRequestException, InternalServerErrorException, NotFoundException, ConflictException } from "@nestjs/common";
 import { CategoriesRepository } from "./categories.repository";
 import { CategoriesBodyDto } from "./dtos/categoriesBodyDto.dto";
 import { Category } from "./categories.entity";
 import slugify from 'slugify';
+import { CategoriesUpdateDto } from "./dtos/categoriesUpdateDto.dto";
+import { UploadApiResponse } from 'cloudinary';
+import { CloudinaryService } from "../cloudinary/cloudinary.service";
 
 @Injectable()
 export class CategoriesService {
-  constructor (private readonly categoriesRepository: CategoriesRepository) {}
+  constructor (private readonly categoriesRepository: CategoriesRepository,
+    private readonly cloudinaryService: CloudinaryService
+  ) {}
 
-  async createCategory({name}: CategoriesBodyDto): Promise<Category> {
+  async createCategory({name}: CategoriesBodyDto, file: Express.Multer.File): Promise<Category> {
     try {
       const categoryExists: Category | null = await this.categoriesRepository.getCategoryByName(name);
       
@@ -16,7 +21,9 @@ export class CategoriesService {
 
       const slug: string = slugify(name, {lower: true, strict: true});
 
-      return await this.categoriesRepository.createCategory(name, slug);
+      const uploadedImage: UploadApiResponse = await this.cloudinaryService.uploadImage(file);
+
+      return await this.categoriesRepository.createCategory(name, uploadedImage.secure_url, uploadedImage.public_id, slug);
       
     } catch (error) {
       console.log(error);
@@ -37,5 +44,30 @@ export class CategoriesService {
     if (!category) throw new NotFoundException('Category not found');
 
     return category;
+  }
+
+  async updateCategory(id: string, {name}: CategoriesUpdateDto, file?: Express.Multer.File): Promise<{message: string}> {
+    const category: Category = await this.getCategoryById(id);
+      
+    if (name) {
+      const categoryExists: Category | null = await this.categoriesRepository.getCategoryByName(name);
+      if (categoryExists && categoryExists.id !== id) throw new ConflictException('Category already exists');
+    }
+  
+    let uploadedImage: UploadApiResponse | undefined;
+    if (file) {
+      uploadedImage = await this.cloudinaryService.uploadImage(file);
+      if (category.imgPublicId) await this.cloudinaryService.deleteImage(category.imgPublicId);
+    }
+  
+    let slug: string | undefined;
+    if (name) {
+      slug = slugify(name, {
+        lower: true,
+        strict: true
+      });
+    }
+  
+    return await this.categoriesRepository.updateCategory(category, name, uploadedImage?.secure_url, uploadedImage?.public_id, slug);
   }
 }
