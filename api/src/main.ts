473dc9b6fe502db80f29dc3d219680e8ad594f1e
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-05-26 04:07:38 -0300

    chore(cors): configure CORS in main.ts to allow frontend requests

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,12 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true,
     stopAtFirstError: true,
   }))
-  await app.listen(process.env.PORT ?? 3000);
+  
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
