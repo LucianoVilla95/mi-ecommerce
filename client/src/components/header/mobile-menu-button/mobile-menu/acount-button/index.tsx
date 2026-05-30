30800ca5140cff725c4eaf7b4595c8598f4f0c26
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-05-29 21:49:34 -0300

    feat(menu): add account button to mobile navigation

---
+++ client/src/components/header/mobile-menu-button/mobile-menu/acount-button/index.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+
+const AcountButton = (): JSX.Element => {
+  return (
+    <button className="flex items-center gap-4 text-lg">
+      Mi cuenta
+    </button>
+  )
+};
+
+export default AcountButton;
