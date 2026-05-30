1aa62f8f13e7f9d95431a235a29a5b9020820d83
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-05-29 21:53:15 -0300

    feat(menu): add logout button to mobile menu

---
+++ client/src/components/header/mobile-menu-button/mobile-menu/login-out-button/index.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+
+const LoginOutButton = (): JSX.Element => {
+  return (
+    <button className="mt-auto bg-neutral-100 rounded-2xl p-4">
+      Cerrar Sesión
+    </button>
+  )
+};
+
+export default LoginOutButton;
