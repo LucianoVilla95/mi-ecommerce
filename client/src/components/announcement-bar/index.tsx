022cb547a265525e852eb6425de012a64f8d2fd6
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-05-29 21:19:31 -0300

    feat: add announcement bar component

---
+++ client/src/components/announcement-bar/index.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+
+const AnnouncementBar = (): JSX.Element => {
+  return (
+    <div className="bg-black text-white text-sm text-center py-3 px-4">
+      🚚 Envíos gratis en compras mayores a $25.000
+    </div>
+  )
+}
+
+export default AnnouncementBar;
