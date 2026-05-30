efb71edec68e4ace9bc292d91b3ce31ec4e2abae
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-05-29 21:31:29 -0300

    feat(header): add brand logo

---
+++ client/src/components/header/logo/index.tsx
@@ -0,0 +1,12 @@
+import { LucideOctagonMinus } from 'lucide-react';
+import { JSX } from 'react';
+
+const Logo = (): JSX.Element => {
+  return (
+    <h1 className="text-3xl font-bold tracking-tight">
+      SHOP APP
+    </h1>
+  )
+};
+
+export default Logo;
