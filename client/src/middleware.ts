2fbbb632f6e0101ef83561ac5ca31ced98131471
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-06-09 01:03:19 -0300

    feat(middleware): add route protection middleware

---
+++ client/src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from 'next/server';
+
+export default function proxy(request: NextRequest) {
+  const token = request.cookies.get('access_token')?.value;
+  const { pathname } = request.nextUrl;
+
+  if ( pathname.startsWith('/auth/signin') || pathname.startsWith('/auth/signup')) {
+    if (token) {
+      return NextResponse.redirect(new URL('/', request.url), { status: 307 });
+    } 
+    return NextResponse.next();
+  }
+  
+  
+  if (!token) return NextResponse.redirect(new URL('/', request.url));
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [ '/cart', '/auth/:path*' ],
+}
