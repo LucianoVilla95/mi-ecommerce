da623710deb056eddf4ba80910201b85e7387044
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-06-09 01:18:51 -0300

    refactor(products): add products component prop types

---
+++ client/src/components/products/types.ts
@@ -0,0 +1,26 @@
+import { Category } from "../categories/category-list/types";
+
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: string;
+  stock: number;
+  imgUrl: string;
+  imgPublicId: string;
+  slug: string;
+  isActive: boolean;
+  category: Category;
+  orderDetails: [];
+}
+
+interface PaginationMeta {
+  total: number,
+  currentPage: number,
+  lastPage: number
+}
+
+export interface PaginationResult<T> {
+  data: T[];
+  meta: PaginationMeta;
+}
