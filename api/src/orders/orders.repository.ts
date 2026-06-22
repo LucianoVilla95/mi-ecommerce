0ae1dddf06ecae6842f4ffd49dd2816014f43397
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-06-21 23:00:01 -0300

    refactor(orders): improve repository methods and fix existing issues

--- api/src/orders/orders.repository.ts
+++ api/src/orders/orders.repository.ts
@@ -12,86 +12,72 @@ export class OrdersRepository {
   @InjectRepository(OrderDetail) private readonly orderDetailsRepository: Repository<OrderDetail>) {}
 
   async getOrderByUser(user: Omit<User, 'password'>, manager?: EntityManager): Promise<Order | null> {
-    return manager ?
-    await manager.getRepository(Order)
-    .createQueryBuilder('order')
-    .innerJoin('order.user', 'user')
-    .innerJoinAndSelect('order.details', 'details')
-    .innerJoinAndSelect('details.product', 'product')
+    const queryBuilder = manager ? manager.getRepository(Order).createQueryBuilder('orders')
+    : this.ordersRepository.createQueryBuilder('orders');
+
+    queryBuilder
+    .innerJoin('orders.user', 'user')
+    .leftJoinAndSelect('orders.details', 'details')
+    .leftJoinAndSelect('details.product', 'product')
     .where('user.id = :userId', { userId: user.id })
-    .andWhere('order.isActive = true')
-    .setLock('pessimistic_write')
-    .getOne()
-    : await this.ordersRepository.findOne({
-      where: {user: {id: user.id}, isActive: true},
-      relations: ['user', 'details', 'details.product']})
+    .andWhere('orders.isActive = true', { isActive: true })
+    if (manager) {
+      queryBuilder.setLock('pessimistic_write', undefined, ['orders']);
+    }
+    return await queryBuilder.getOne();
   }
 
-  async createOrder(user: Omit<User, 'password'>): Promise<Order> {
-    const order: Order = await this.ordersRepository.create({user});
-    return await this.ordersRepository.save(order);
+  async createOrder(user: Omit<User, 'password'>, manager?: EntityManager): Promise<Order> {
+    const order: Order = this.ordersRepository.create({user});
+    const repo = manager ? manager.getRepository(Order) : this.ordersRepository;
+    return await repo.save(order);
   }
 
   async updateOrder(order: Order, manager?: EntityManager): Promise<{message: string}> {
-    manager ?
-    await manager.getRepository(Order).save(order)
-    : await this.ordersRepository.save(order);
-
+    const repo = manager ? manager.getRepository(Order) : this.ordersRepository;
+    await repo.save(order);
     return {
       message: 'Order updated successfully'
     }
   }
 
   async createOrderDetail(order: Order, product: Product, quantity: number, price: number, manager?: EntityManager): Promise<OrderDetail> {
-    return manager ?
-    await manager?.getRepository(OrderDetail).save({order, product, quantity, price})
-    : await this.orderDetailsRepository.save({order, product, quantity, price});
+    const repo = manager ? manager.getRepository(OrderDetail) : this.orderDetailsRepository;
+    return await repo.save({order, product, quantity, price});
   }
 
   async getOrderDetail(order: Order, product: Product, manager?: EntityManager): Promise<OrderDetail | null> {
-    return manager ?
-    await manager.getRepository(OrderDetail).findOne({
-      where: {order: {id: order.id}, product: {id: product.id}},
-      relations: ['order', 'product']})
-    : await this.orderDetailsRepository.findOne({
+    const repo = manager ? manager.getRepository(OrderDetail) : this.orderDetailsRepository;
+    return await repo.findOne({
       where: {order: {id: order.id}, product: {id: product.id}},
       relations: ['order', 'product']});
   }
 
   async updateOrderDetail(orderDetailExisting: OrderDetail, manager?: EntityManager): Promise<{message: string}> {
-    manager ?
-    await manager.getRepository(OrderDetail).save(orderDetailExisting)
-    : await this.orderDetailsRepository.save(orderDetailExisting);
-
+    const repo = manager ? manager.getRepository(OrderDetail) : this.orderDetailsRepository;
+    await repo.save(orderDetailExisting);
     return {
       message: 'Product added successfully!'
     }
   }
 
-  async deleteOrderDetail(order: Order, id: string, manager?: EntityManager): Promise<DeleteResult> {
-    return manager ?
-    await manager.getRepository(OrderDetail).delete({
-      order: {id: order.id},
-      product: {id: id}})
-    : await this.orderDetailsRepository.delete({
-      order: {id: order.id},
-      product: {id: id}
-    });
+  async deleteOrderDetail(id: string, manager?: EntityManager): Promise<DeleteResult> {
+    const repo = manager ? manager.getRepository(OrderDetail) : this.orderDetailsRepository;
+    return await repo.delete(id);
   }
 
   async getOrderById(id?: string, manager?: EntityManager): Promise<Order | null> {
-    return manager ?
-    await manager
-    .getRepository(Order)
-    .createQueryBuilder('order')
-    .innerJoinAndSelect('order.user', 'user')
-    .innerJoinAndSelect('order.details', 'details')
-    .innerJoinAndSelect('details.product', 'product')
-    .setLock('pessimistic_write')
-    .where('order.id = :id', { id })
-    .getOne()
-    : await this.ordersRepository.findOne({
-      where: { id, isActive: true },
-      relations: ['user', 'details', 'details.product']});
+    const queryBuilder = manager ? manager.getRepository(Order).createQueryBuilder('orders')
+    : this.ordersRepository.createQueryBuilder('orders');
+    
+    queryBuilder
+    .innerJoinAndSelect('orders.user', 'user')
+    .leftJoinAndSelect('orders.details', 'details')
+    .leftJoinAndSelect('details.product', 'product')
+    .where('orders.id = :id', { id });
+    if (manager) {
+      queryBuilder.setLock('pessimistic_write', undefined, ['orders']);
+    }
+    return await queryBuilder.getOne();
   }
 }
