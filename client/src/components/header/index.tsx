c6d852e5941e9e298f220e4027f71551c3719399
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-06-28 03:41:54 -0300

    feat(header): render auth actions based on user session

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,13 +1,24 @@
-import { JSX } from 'react';
-import MenuButton from './mobile-menu-button';
+import { JSX, Suspense } from 'react';
 import Logo from './logo';
 import CartButton from './cart-button';
+import { cookies } from 'next/headers'; 
+import SearchBar from './search-bar';
+import AuthButton from './auth-button';
+import LogoutButton from './auth-button/logout-button';
 
-const Header = (): JSX.Element => {
+const Header = async (): Promise<JSX.Element> => {
+  const cookieStore = await cookies();
+  const isAuthenticated: boolean = cookieStore.has('access_token');
+
   return (
-    <header className="px-5 h-16 flex items-center justify-between">
-      <MenuButton />
-      <Logo />      
+    <header className="max-w-7xl mx-auto px-5 h-16 border-b flex items-center justify-between gap-4" >
+      <Logo />
+      <Suspense>
+        <SearchBar />
+      </Suspense>
+      {
+        isAuthenticated ? <LogoutButton /> : <AuthButton /> 
+      }
       <CartButton />
     </header>
   )
