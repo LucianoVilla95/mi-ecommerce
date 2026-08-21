3703ed8ff49d2c7d91d96ff558f68e65f2f0ba8a
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-08-21 00:20:36 -0300

    docs(orders): add JSDoc comment and validation to AddToCartDto

--- api/src/orders/dtos/addToCartDto.dto.ts
+++ api/src/orders/dtos/addToCartDto.dto.ts
@@ -3,6 +3,9 @@ import { IsArray, ValidateNested } from "class-validator";
 import { Type } from 'class-transformer';
 
 export class AddToCartDto {
+  /**
+   * Lista de productos y sus respectivas cantidades que el usuario tiene en su carrito local.
+   */
   @IsArray({ message: 'Items must be an arrangement' })
   @ValidateNested({ each: true })
   @Type(() => AddProductsDto)
