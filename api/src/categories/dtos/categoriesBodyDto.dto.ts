c553b173ab81bf2a4cf171c35aed3f1dac75f519
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-09-06 07:41:31 -0300

    refactor(categories): clean up original category DTO by removing Swagger comments

--- api/src/categories/dtos/categoriesBodyDto.dto.ts
+++ api/src/categories/dtos/categoriesBodyDto.dto.ts
@@ -1,10 +1,7 @@
 import { IsString, IsNotEmpty } from "class-validator";
 
 export class CategoriesBodyDto {
-  /**
-   * Nombre único de la categoría de productos
-   * @example "Electrónica"
-   */
+
   @IsNotEmpty()
   @IsString()
   name!: string;
