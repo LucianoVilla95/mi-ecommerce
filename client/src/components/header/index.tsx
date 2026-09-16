79c23fda7aa464270bb2ddb1e5823dd7cb063598
Luciano Villarroel <88258891+LucianoVilla95@users.noreply.github.com.>
2026-09-16 14:32:55 -0300

    feat(header): add Button component

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -5,6 +5,7 @@ import { cookies } from 'next/headers';
 import SearchBar from './search-bar';
 import AuthButton from './auth-button';
 import LogoutButton from './auth-button/logout-button';
+import MenuButton from './mobile-menu-button';
 
 const Header = async (): Promise<JSX.Element> => {
   const cookieStore = await cookies();
@@ -12,12 +13,15 @@ const Header = async (): Promise<JSX.Element> => {
 
   return (
     <header className="max-w-7xl mx-auto px-5 h-16 border-b flex items-center justify-between gap-4" >
+      <MenuButton isLoggedIn={isAuthenticated} />
       <Logo />
-      <Suspense>
-        <SearchBar />
-      </Suspense>
+      <div className="hidden sm:block flex-1 max-w-lg">
+        <Suspense>
+          <SearchBar />
+        </Suspense>
+      </div>
       {
-        isAuthenticated ? <LogoutButton /> : <AuthButton /> 
+        isAuthenticated ? <div className="hidden md:inline-block"><LogoutButton /></div> : <AuthButton /> 
       }
       <CartButton />
     </header>
